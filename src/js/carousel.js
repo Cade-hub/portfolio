d683ee6d71a5c1ba96290dad3ab0296313fa43cb
Cade Wickham <25160931@sun.ac.za>
2023-10-27 17:31:34 +0200

    homepage and started design carousel

---
+++ src/js/carousel.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        // Slick Carousel options here
+        arrows: true, // Show navigation arrows
+        dots: true,   // Show navigation dots
+        autoplay: true, // Autoplay the carousel
+        autoplaySpeed: 2000, // Autoplay speed in milliseconds (adjust as needed)
+    });
+});
