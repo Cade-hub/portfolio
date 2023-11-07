55e6adc7dd851f262d1ef0a05d40b8c20a0cde96
Cade Wickham <25160931@sun.ac.za>
2023-11-07 22:35:38 +0200

    surely

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Navigation from "./navigation";
 import imagesLoaded from '/node_modules/imagesloaded/imagesloaded.js';
-import WebFont from 'webfontloader';
+import WebFont from '/node_modules/webfontloader/webfontloader.js';
 
 // Preload images
 const preloadImages = () => {
